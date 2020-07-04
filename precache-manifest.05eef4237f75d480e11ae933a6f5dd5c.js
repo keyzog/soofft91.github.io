18cab85b402ef325d5bc6e9c75cf76bfa322adb2
soofft91 <soofft91@users.noreply.github.com>
2020-07-04 17:35:03 +0300

    Add files via upload

---
+++ precache-manifest.05eef4237f75d480e11ae933a6f5dd5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26083eecafdf7a69baad8a053a64a61d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0611eb7f2ebc038ae75",
+    "url": "/static/css/main.5aa790f1.chunk.css"
+  },
+  {
+    "revision": "8325ec327e3e6d0c3d66",
+    "url": "/static/js/2.e687382a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e687382a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0611eb7f2ebc038ae75",
+    "url": "/static/js/main.53d4bfb2.chunk.js"
+  },
+  {
+    "revision": "8086204bb1f594bfbb6e",
+    "url": "/static/js/runtime-main.57169b17.js"
+  }
+]);
